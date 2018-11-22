47fba0b1edf9355412cb40a142b9493a344cdd32
dezzra <45214373+dezzra@users.noreply.github.com>
2018-11-22 13:50:34 -0700

    Add files via upload

---
+++ caesarCipher.py
@@ -0,0 +1,50 @@
+# Caesar Cipher
+
+import pyperclip
+
+#the string to be encrypted/decrypted
+message = input('Enter a message: ')
+
+# the encryption/decryption key
+key = 13
+
+# tells the program to encrypt or decrypt
+mode = 'decrypt'
+
+# every possible symbol that can be encrypted
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# stores the encrypted/decrypted form of the message
+translated = ''
+
+# capitalize the string in message
+message = message.upper()
+
+# run the encryption/decryption code on each symbol in the message string
+for symbol in message:
+    if symbol in LETTERS:
+        # get the encrypted (or decrypted) number for this symbol
+        num = LETTERS.find(symbol)
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+
+        # handle wrap-around if num is larger than the length of LETTERS or < 0
+
+        if num >= len(LETTERS):
+            num = num - len(LETTERS)
+        elif num < 0:
+            num = num + len(LETTERS)
+
+        # add encrypted/decrypted number's symbol at the end of translated
+        translated = translated + LETTERS[num]
+    else:
+        # just add the symbol without encrypting/decrypting
+        translated = translated + symbol
+
+# print the encrypted/decrypted string to the screen
+print(translated)
+
+#copy the encrypted/decrypted string to the clipboard
+pyperclip.copy(translated)
