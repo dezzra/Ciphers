2c2553edbd542dd4f7adace0b9f42dede38d7210
dezzra <45214373+dezzra@users.noreply.github.com>
2018-12-07 08:06:52 -0700

    Update caesarCipher.py
    Add labels and buttons in a grid

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,13 +1,12 @@
 # Caesar Cipher
-
-# Caesar Cipher
 from tkinter import *
 
 window = Tk()
+window.title('Caesar Cipher')
 
-#the string to be encrypted/decrypted
-message = input('Enter a message: ')
-message = message.upper()
+#display the string to be encrypted/decrypted
+#add more code here to do that
+#message = message.upper()
 
 # the encryption/decryption key
 key = 22
@@ -15,13 +14,14 @@ key = 22
 # every possible symbol that can be encrypted
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# stores the encrypted/decrypted form of the message
-translated = ''
-
 # run the encryption/decryption code on each symbol in the message string
-def encrypt(message):
+def encrypt(msg):
     global key, translated
-    for symbol in message:
+    translated = ''
+    msg = msg.upper()
+    lbl_origMsg.configure(text = msg)
+    lbl_enc.configure(text = 'Encrypted Message:')
+    for symbol in msg:
         if symbol in LETTERS:
             num = LETTERS.find(symbol)
             num = num + key
@@ -31,10 +31,17 @@ def encrypt(message):
             translated = translated + LETTERS[num]
         else:
             translated = translated + symbol
+    lbl_encMsg.configure(text = translated)
+    print('Original message:', msg)
+    print('Encrypted message:', translated)
 
-def decrypt(message):
+def decrypt(msg):
     global key, translated
-    for symbol in message:
+    translated = ''
+    msg = msg.upper()
+    lbl_origMsg.configure(text = msg)
+    lbl_enc.configure(text = 'Decrypted Message:')
+    for symbol in msg:
         if symbol in LETTERS:
             num = LETTERS.find(symbol)
             num = num - key
@@ -44,20 +51,45 @@ def decrypt(message):
             translated = translated + LETTERS[num]
         else:
             translated = translated + symbol
-    #add code to update label here
+    lbl_encMsg.configure(text = translated)
+    print('Original message:', msg)
+    print('Decrypted message:', translated)
 
-print(translated)
+def reset():
+    global translated
+    translated = ''
+    lbl_origMsg.configure(text = '')
+    lbl_encMsg.configure(text = '')
+    pass
 
-window.title('Caesar Cipher')
+def about():
+    # add code to explain what this program does, and how, in another popup.
+    pass
+
+entry = Entry(window)
+
+message = entry.get()
+
 lbl_msg = Label(window, text = 'Message:')
 lbl_orig = Label(window, text = 'Original Message:')
+lbl_origMsg = Label(window, text = 'a')
 lbl_enc = Label(window, text = 'Encrypted Message:')
-enc_btn = Button(window, text = 'Encrypt', command=encrypt)
-dec_btn = Button(window, text = 'Decrypt', command=decrypt)
+lbl_encMsg = Label(window, text = 'b')
+enc_btn = Button(window, text = 'Encrypt', command=encrypt(message))
+dec_btn = Button(window, text = 'Decrypt', command=decrypt(message))
+res_btn = Button(window, text = 'Reset', command=reset())
+about_btn = Button(window, text = 'About', command=about())
 
-lbl_msg.pack(side = LEFT)
-lbl_orig.pack(side = LEFT)
-lbl_enc.pack(side = LEFT)
-enc_btn.pack(side = RIGHT)
-dec_btn.pack(side = RIGHT)
+entry.grid(row = 1, column = 2)
+lbl_msg.grid(row = 1, column = 1)
+lbl_orig.grid(row = 2, column = 1)
+lbl_origMsg.grid(row = 2, column = 2)
+lbl_enc.grid(row = 3, column = 1)
+lbl_encMsg.grid(row = 3, column = 2)
+enc_btn.grid(row = 1, column = 3, rowspan = 2)
+dec_btn.grid(row = 1, column = 4, rowspan = 2)
+res_btn.grid(row = 3, column = 3)
+about_btn.grid(row = 3, column = 4)
+
 window.mainloop()
+mainloop()
